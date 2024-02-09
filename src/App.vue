f710b4ff0866adf61cd4fb9ad50e0dec001851f4
lokeshsadhu <lokeshsrinivas1861@gmail.com>
2024-02-09 17:35:25 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <label for="userInput">Enter Text:</label>
+    <input v-model="inputText" id="userInput" />
+    <button @click="handleSubmit">Submit</button>
+
+    <div v-if="submittedText">
+      <p>Submitted Text:</p>
+      <p>{{ submittedText }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const inputText = ref('');
+    const submittedText = ref('');
+
+    const handleSubmit = () => {
+      submittedText.value = inputText.value;
+    };
+
+    return {
+      inputText,
+      submittedText,
+      handleSubmit,
+    };
+  },
+};
+</script>
